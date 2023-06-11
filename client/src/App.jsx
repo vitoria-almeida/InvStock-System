7f3dbeb402c919523fe836a42df6b03c0940cbcd
Vitoria-m <vitoriazzz@outlook.com>
2023-06-11 14:47:53 -0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const renderTitulo = () => {
+    return <h1>Renderizando Título</h1>
+  }
+  return (<>
+      <div>
+        {renderTitulo()}
+      </div>
+  </>)
+}
+
+export default App
