442436613439ae64d8f1036a00a67279a4995274
vitoria-almeida <vitoria.dev@outlook.com>
2023-09-19 17:25:36 -0300

    Método patch: nova rota e método de atualização

--- server/routes/AuthRegisterUserRoutes.js
+++ server/routes/AuthRegisterUserRoutes.js
@@ -5,5 +5,6 @@ router.get('/', AuthRegisterUserController.init)
 router.post('/auth/register/user', AuthRegisterUserController.registerUser)
 router.get('/auth/users', AuthRegisterUserController.findAllUsers)
 router.get('/auth/user/:id', AuthRegisterUserController.getUserById)
+router.patch('/auth/user/:id', AuthRegisterUserController.updateUser)
 
 module.exports = router
