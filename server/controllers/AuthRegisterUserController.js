f40d456e68c9f880b3d65f930c0250a7a2f4c6b6
vitoria-almeida <vitoria.dev@outlook.com>
2023-09-20 17:30:01 -0300

    Adicionando try catch nas funções/métodos

--- server/controllers/AuthRegisterUserController.js
+++ server/controllers/AuthRegisterUserController.js
@@ -7,66 +7,74 @@ module.exports = class AuthRegisterUserController {
     } 
 
     static async registerUser(req, res) {
-        const { name, email, password, confirmPassword } = req.body
-
-        if(!name) {
-            return res.status(422).json({message: 'O nome é obrigatório'})
-        }
-        if(!email) {
-            return res.status(422).json({message: 'O email é obrigatório'})
-        }
-        if(!password) {
-            return res.status(422).json({message: 'A senha é obrigatória'})
-        }
-        if(password != confirmPassword) {
-            return res.status(422).json({message: 'As senhas não coincidem'})
-        }
-
-        const userExist = await User.findOne({email: email})
-        if(userExist) {
-            return res.status(422).json({message: 'Já existe uma conta cadastrada com este email'})
-        }
-
-        const hash = await bcrypt.genSalt(12)
-
-        const hashPassword = await bcrypt.hash(password, hash)
-        
-        const user = new User({
-            name,
-            email,
-            password: hashPassword
-        })
-
         try {
+            const { name, email, password, confirmPassword } = req.body
+
+            if(!name) {
+                return res.status(422).json({message: 'O nome é obrigatório'})
+            }
+            if(!email) {
+                return res.status(422).json({message: 'O email é obrigatório'})
+            }
+            if(!password) {
+                return res.status(422).json({message: 'A senha é obrigatória'})
+            }
+            if(password != confirmPassword) {
+                return res.status(422).json({message: 'As senhas não coincidem'})
+            }
+
+            const userExist = await User.findOne({email: email})
+            if(userExist) {
+                return res.status(422).json({message: 'Já existe uma conta cadastrada com este email'})
+            }
+
+            const hash = await bcrypt.genSalt(12)
+
+            const hashPassword = await bcrypt.hash(password, hash)
+            
+            const user = new User({
+                name,
+                email,
+                password: hashPassword
+            })
+
             await user.save()
-            res.status(201).json({message: 'Usuário cadastrado com sucesso!', user})
+
+            res.status(201).json({message: 'Usuário cadastrado com sucesso!', user})         
         } catch(error) {
-            res.status(500).json({message: 'Ocorreu um erro ao cadastrar o usuário, tente novamente.'})
+            res.status(500).json({message: error.message})
         }
     }
 
     static async findAllUsers(req, res) {
-        const allUsers = await User.find()
+        try {
+            const allUsers = await User.find()
 
-        if(allUsers.length === 0) {
-            return res.status(400).json({message: 'Não há usuários registrados'})
+            if(allUsers.length === 0) {
+                return res.status(400).json({message: 'Não há usuários registrados'})
+            }
+
+            res.status(200).json({allUsers})
+        } catch(error) {
+            res.status(500).json({message: error.message})
         }
-
-        res.status(200).json({allUsers})
     }
 
     static async getUserById(req, res) {
-        const user = req.user
-        res.status(200).json({user})
+        try {
+            const user = req.user
+            res.status(200).json({user})
+        } catch(error) {
+            res.status(500).json({message: error.message})
+        }
     }
 
     static async updateUser(req, res) {
-        const { name, email, password, confirmPassword } = req.body
-        if(!name && !email && !password && !confirmPassword) {
-            return res.status(422).json({message: 'Preencha ao menos um item a ser atualizado'})
-        }
-
         try {
+            const { name, email, password, confirmPassword } = req.body
+            if(!name && !email && !password && !confirmPassword) {
+                return res.status(422).json({message: 'Preencha ao menos um item a ser atualizado'})
+            }
             const user = req.user
 
             if (name) {
@@ -88,7 +96,7 @@ module.exports = class AuthRegisterUserController {
 
             res.status(201).json({message: 'Usuário atualizado com sucesso!', user})
         } catch(error) {
-            res.status(500).json({message: 'Ocorreu um erro ao tentar atualizar o usuário, tente novamente.'})
+                res.status(500).json({message: error.message})
         }
     }
 }
--- server/middlewares/GlobalMiddlewares.js
+++ server/middlewares/GlobalMiddlewares.js
@@ -2,28 +2,36 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 const validId = (req, res, next) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({message: 'ID Inválido'})
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({message: 'ID Inválido'})
+        }
+
+        next()
+    } catch(error) {
+        res.status(500).json({message: error.message})
     }
-
-    next()
 }
 
 const validUser = async (req, res, next) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
     
-    const user = await User.findById(id, '-password')
+        const user = await User.findById(id, '-password')
 
-    if(!user) {
-        return res.status(400).json({message: 'Usuário não encontrado'})
+        if(!user) {
+            return res.status(400).json({message: 'Usuário não encontrado'})
+        }
+
+        req.id = id
+        req.user = user
+
+        next()
+    } catch(error) {
+        res.status(500).json({message: error.message})
     }
-
-    req.id = id
-    req.user = user
-
-    next()
 }
 
 module.exports = {validId, validUser}
