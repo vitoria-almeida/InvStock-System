8eafa990d179f91915818cc40f30b8e7aad494d1
vitoria-almeida <vitoria.dev@outlook.com>
2023-10-10 19:51:58 -0300

    Cria paginação de dados na busca do banco de dados

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -34,15 +34,61 @@ class ProductsController {
         }
     }
 
+    static async countProducts(req, res) {
+        try {
+            const countAllProducts = await Product.countDocuments()
+            return countAllProducts
+        } catch(error) {
+            res.status(500).json({message: error.message})
+        }
+    }
+
     static async findAllProducts(req, res) {
         try {
-            const allProducts = await Product.find()
+            let { limit, offset } = req.query
 
-            if(allProducts.length === 0) {
+            limit = Number(limit)
+            offset = Number(offset)
+
+            if(!limit) {
+                limit = 5
+            } if(!offset) {
+                offset = 0
+            }
+
+            const allProducts = await Product.find().skip(offset).limit(limit).populate('userId')
+
+            const total = await ProductsController.countProducts()
+
+            const currentUrl = req.baseUrl
+
+            const next = offset + limit
+            const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null
+        
+            const previous = offset - limit < 0 ? null : offset - limit
+            const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null
+
+            if (allProducts.length === 0) {
                 return res.status(400).json({message: 'Não há produtos registrados'})
             }
 
-            res.status(200).json({allProducts})         
+            res.status(200).json({
+                nextUrl,
+                previousUrl,
+                limit,
+                offset,
+                total,
+
+                results: allProducts.map(item => ({
+                    id: item._id,
+                    name: item.productName,
+                    quantity: item.quantity,
+                    price: item.price,
+                    selling: item.sellingPrice,
+                    date: item.createdAt,
+                    user: item.userId
+                }))
+            })         
         } catch(error) {
             res.status(500).json({message: error.message})
         }
